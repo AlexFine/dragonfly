ee5b1a192cd90e5c7130951bd7106f24591d50ea
AlexFine <alexkfine2@gmail.com>
2015-07-30 09:52:00 -0700

    everything so far

---
+++ myApp/www/js/services.js
@@ -0,0 +1,21 @@
+angular.module('starter.services', ['ionic', 'ngCordova'])
+    .service('search', ['$http', 'photos', function ($http, photos) {
+        var facts = [];
+        return {
+            getFacts: function (query) {
+
+                $http.get("http://dragonflysearch.com/api/search.php?q=" + query)
+                    .then(function (resp) {
+                        if (resp.data.Facts) {
+                            dflynotes = resp.data;
+                            facts.push(dflynotes.Facts[0]);
+                            return facts;
+                        }
+                    }, function (err) {
+                        if (!resp.data.facts) {
+                            console.log('no info on ' + query + ' yet :)');
+                        }
+                    })
+            }
+        }
+}])
